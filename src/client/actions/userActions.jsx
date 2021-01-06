fcf8a16e44b292b5a223904a26e4b4af8f455d96
Clark Oake <clarkoake13@gmail.com>
2021-01-06 15:39:31 -0330

    adding email and username validation to the user creation process
    Still needs work to get 100% functional

--- src/client/actions/userActions.jsx
+++ src/client/actions/userActions.jsx
@@ -2,48 +2,80 @@ import actionTypes from "./actionTypes";
 import userAPI from "../api/userAPI";
 
 export function createUser(user) {
-    return function (dispatch) {
-      // validate unique email address here
-      return userAPI.createUser(user).then(resUser => {
-        dispatch(createUserSuccess(resUser));
-      }).catch(error => {
-        throw(error);
+  return function (dispatch) {
+    // Validate unique username is entered
+    return userAPI.getUserbyName(user)
+      .then(resUser => {
+        if(resUser.name.len > 0) {
+          alert(`That user name is already in use, please enter a different username and try again`);
+          return createUserFailure();
+        }
+      })
+      .catch(err => {
+        //Validate unique email address is entered
+        userAPI.getUserbyEmail(user)
+          .then(resUser => {
+            if(resUser.email.len > 0) {
+              dispatch(createUserFailure());
+              alert(`That email address is already registered to an account, please use a different email address and try again`);
+              return;
+            };
+          })
+          .catch(err => {
+            //Create unique user in DB after validating unique email and username
+            userAPI.createUser(user)
+              .then((resUser) => {
+                dispatch(createUserSuccess(resUser));
+              })
+              .catch((error) => {
+                throw error;
+              });
+          });
       });
-    };
-  }
+  };
+};
 
-export const createUserSuccess = user => {
-  return { 
-      type: actionTypes.CREATE_USER_SUCCESS, 
-      payload: user, 
-    };
+export const createUserSuccess = (user) => {
+  return {
+    type: actionTypes.CREATE_USER_SUCCESS,
+    payload: user,
+  };
+};
+
+export const createUserFailure = (user) => {
+  return {
+    type: actionTypes.CREATE_USER_FAILURE,
+    payload: user,
+  };
 };
 
 export const clearUserSuccess = () => {
   return {
     type: actionTypes.CLEAR_USER_SUCCESS,
-  }
+  };
 };
 
-export const getUserbyName = user => {
+export const getUserbyName = (user) => {
   return function (dispatch) {
-    return userAPI.getUserbyName(user)
-      .then(resUser => {
+    return userAPI
+      .getUserbyName(user)
+      .then((resUser) => {
         dispatch(getUserSuccess(resUser, user));
       })
-      .catch(err => {
+      .catch((err) => {
         throw err;
       });
   };
 };
 
-export const getUserbyEmail = user => {
+export const getUserbyEmail = (user) => {
   return function (dispatch) {
-    return userAPI.getUserbyEmail(user)
-      .then(resUser => {
-        dispatch(getUserSuccess(resUser, user)
-      )})
-      .catch(err => {
+    return userAPI
+      .getUserbyEmail(user)
+      .then((resUser) => {
+        dispatch(getUserSuccess(resUser, user));
+      })
+      .catch((err) => {
         throw err;
       });
   };
@@ -56,51 +88,53 @@ export const getUserSuccess = (resUser, user) => {
   } else resUser.active = false;
   // console.log(resUser.password, user.password);
   if (resUser.password === user.password) {
-    return { 
-      type: actionTypes.GET_USER_SUCCESS, 
-      payload: resUser, 
+    return {
+      type: actionTypes.GET_USER_SUCCESS,
+      payload: resUser,
     };
   } else {
-      return { 
-       type: actionTypes.USER_LOGIN_FAILED,
-      };
+    return {
+      type: actionTypes.USER_LOGIN_FAILED,
     };
-  };
+  }
+};
 
 export const updateUserbyId = (user) => {
-    return async dispatch => {
-        return userAPI.updateUserbyId(user)
-          .then(users => {
-            dispatch(updateUserSuccess(users));
-          })
-          .catch(err => {
-            throw err;
-          });
-      };
+  return async (dispatch) => {
+    return userAPI
+      .updateUserbyId(user)
+      .then((users) => {
+        dispatch(updateUserSuccess(users));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
 };
 
-export const updateUserSuccess = user => {
-    return { 
-        type: actionTypes.GET_USER_SUCCESS, 
-        payload: user 
-    };
-}
-
-export const deleteUserbyId = user => {
-    return async dispatch => {
-        return userAPI.deleteUserbyId(user)
-          .then(users => {
-            dispatch(deleteUserSuccess(users));
-          })
-          .catch(err => {
-            throw err;
-          });
-      };
+export const updateUserSuccess = (user) => {
+  return {
+    type: actionTypes.GET_USER_SUCCESS,
+    payload: user,
+  };
 };
 
-export const deleteUserSuccess = user => {
-    return { 
-        type: actionTypes.GET_USER_SUCCESS, 
-        payload: user 
-    };
-}
+export const deleteUserbyId = (user) => {
+  return async (dispatch) => {
+    return userAPI
+      .deleteUserbyId(user)
+      .then((users) => {
+        dispatch(deleteUserSuccess(users));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+};
+
+export const deleteUserSuccess = (user) => {
+  return {
+    type: actionTypes.GET_USER_SUCCESS,
+    payload: user,
+  };
+};
--- src/client/reducers/userReducer.jsx
+++ src/client/reducers/userReducer.jsx
@@ -39,6 +39,18 @@ const userCreateCompleted = (state, action) => {
   };
 };
 
+const userCreateFailed = (state, action) => {
+  return {
+    ...state,
+    users: {
+      'user1': {
+        userName: null,
+        isLogged: false
+      }
+    }
+  };
+};
+
 const userLogoutCompleted = (state) => {
   return {
     ...state,
@@ -89,6 +101,9 @@ const userReducer = (state = initialState, action) => {
       return userLogoutCompleted(state, action);
     case actionTypes.USER_LOGIN_FAILED:
       return userLoginFailed(state, action);
+    case actionTypes.CREATE_USER_FAILURE:
+      return userCreateFailed(state, action);
+    
     // case actionTypes.UPDATE_USER_SUCCESS:
     //   return userUpdateCompleted(state, action);
     // case actionTypes.DELETE_USER_SUCCESS:
